b82c88103b061e5df76b5aa8f1d7dd000bb4d0ae
Marcos Soares <francicomarcossoaresbarros@gmail.com>
2026-08-14 14:54:47 -0300

    Update index.js
    Marcos Soares

--- 1-bimestre/logica-de-programacao/aula08/index.js
+++ 1-bimestre/logica-de-programacao/aula08/index.js
@@ -1,5 +1,16 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+const nomeCompleto = "Maria luiza"
+const idadeAluno = 15
+const cidade = general sampaio
+
+const nomeCompleto = "marcos soares"
+const idadeAluno  = 11
+const cidade = vila são joão
+
+const nomeCompleto = "liz silva"
+const idadeAluno = 14
+const cidade = pacoti
 
 
 
