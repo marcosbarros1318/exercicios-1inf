f71dd999ba0e59454a3754a4f11a9cc816031588
Marcos Soares <francicomarcossoaresbarros@gmail.com>
2026-09-01 21:05:08 -0300

    Atualizar o index.js

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,32 +1,33 @@
-// CRIE SUA SOLUÇÃO ABAIXO ================
-
+// CRIE SUA SOLUÇÃO ABAIXO ===============
 const cliente = "Mariana Silva";
 const cidade = "Fortaleza";
 const produto = "Notebook Pro";
 const categoria = "Notebook";
 const preco = 3500;
 const quantidade = 2;
-const descontoPersentual = 10;
+const descontoPercentual = 10;
 const valorPago = 7000;
 
 const subtotal = preco * quantidade;
-const valorDesconto = subtotal * (descontoPercentual / 100;
+const valorDesconto = subtotal * (descontoPercentual / 100);
 const valorFinal = subtotal - valorDesconto;
-const troco = valorpago - valorFinal;
-
+const troco = valorPago - valorFinal;
 
 console.log("===== RESUMO DO PEDIDO =====");
 console.log(`Cliente: ${cliente}`);
 console.log(`Cidade: ${cidade}`);
 console.log(`Produto: ${produto}`);
 console.log(`Categoria: ${categoria}`);
-console.log(`Preço unitaria: R$ ${preco.toFixed(2)}`);
+console.log(`Preço unitário: R$ ${preco.toFixed(2)}`);
 console.log(`Quantidade: ${quantidade}`);
-console.log(`Desconto: ${descontoPercentual)%`);
+console.log(`Desconto: ${descontoPercentual}%`);
 console.log(`Valor do desconto: R$ ${valorDesconto.toFixed(2)}`);
 console.log(`Valor final: R$ ${valorFinal.toFixed(2)}`);
 console.log(`Valor pago: R$ ${valorPago.toFixed(2)}`);
 console.log(`Troco: R$ ${troco.toFixed(2)}`);
+
+
+
 -// === FIM DO CÓDIGO==================
  -// === NÃO FAZER NADA ABAIXO DESSA LINHA ==
 -module.exports = {
